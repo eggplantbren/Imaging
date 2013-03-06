7bd3890540da9d521af72888148408471779fe81
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-03-06 14:15:00 +1300

    Made Array a class template so we can have e.g. int arrays

--- Array.h
+++ Array.h
@@ -6,11 +6,12 @@
 namespace Imaging
 {
 
+template<class Type>
 class Array
 {
 	protected:
 		int ni, nj, size;
-		std::vector< std::vector<double> > pixels;
+		std::vector< std::vector<Type> > pixels;
 
 	public:
 		Array();
@@ -20,23 +21,24 @@ class Array
 		void resize(int ni, int j);
 
 		// Set all pixels to given value
-		void set(double value);
-		void increment(double value);
-		void decrement(double value);
+		void set(const Type& value);
+		void increment(const Type& value);
+		void decrement(const Type& value);
 
 		// Operator versions of the above
-		Array& operator  = (double value) { set(value); return *this; }
-		Array& operator += (double value) { increment(value); return *this; }
-		Array& operator -= (double value) { decrement(value); return *this; }
+		Array& operator  = (const Type& value) { set(value); return *this; }
+		Array& operator += (const Type& value) { increment(value); return *this; }
+		Array& operator -= (const Type& value) { decrement(value); return *this; }
 
 		// Const and non-const getter
-		double operator() (int i, int j) const { return pixels[i][j]; }
-		double& operator() (int i, int j) { return pixels[i][j]; }
+		Type operator()  (int i, int j) const { return pixels[i][j]; }
+		Type& operator() (int i, int j)       { return pixels[i][j]; }
 
 };
 
 }
 
-#endif
+#include "ArrayImpl.h"
 
+#endif
 
--- ArrayImpl.h
+++ ArrayImpl.h
@@ -1,47 +1,51 @@
-#include "Array.h"
-
 namespace Imaging
 {
 
 using namespace std;
 
-Array::Array()
+template<class Type>
+Array<Type>::Array()
 :ni(0), nj(0)
 ,size(0)
 {
 
 }
 
-Array::Array(int ni, int nj)
+template<class Type>
+Array<Type>::Array(int ni, int nj)
 :ni(ni), nj(nj)
 ,size(ni*nj)
-,pixels(ni, vector<double>(nj))
+,pixels(ni, vector<Type>(nj))
 {
 
 }
 
-void Array::resize(int ni, int nj)
+template<class Type>
+void Array<Type>::resize(int ni, int nj)
 {
 	this->ni = ni; this->nj = nj;
 	size = ni*nj;
-	pixels.resize(ni, vector<double>(nj));
+	pixels.resize(ni, vector<Type>(nj));
 }
 
-void Array::set(double value)
+template<class Type>
+void Array<Type>::set(const Type& value)
 {
 	for(int i=0; i<ni; i++)
 		for(int j=0; j<nj; j++)
 			pixels[i][j] = value;
 }
 
-void Array::increment(double value)
+template<class Type>
+void Array<Type>::increment(const Type& value)
 {
 	for(int i=0; i<ni; i++)
 		for(int j=0; j<nj; j++)
 			pixels[i][j] += value;
 }
 
-void Array::decrement(double value)
+template<class Type>
+void Array<Type>::decrement(const Type& value)
 {
 	for(int i=0; i<ni; i++)
 		for(int j=0; j<nj; j++)
